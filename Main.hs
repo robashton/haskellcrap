ca2c8f8cfbe8125f237108daeb089d3d2b53408a
Rob Ashton <robashton@codeofrob.com>
2014-04-26 19:02:44 +0100

    Nother function

--- Main.hs
+++ Main.hs
@@ -3,3 +3,9 @@ module Main where
 main :: IO()
 main = do putStrLn "What the hell yo"
           putStrLn "This is a thing"
+          whatever
+
+
+whatever :: IO()
+whatever = do putStrLn "WTF"
+              putStrLn "Fold this?"
